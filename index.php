4bd38db15e1f27a85b064d016f0b6725b3b59597
Caskey, Damon V <dc@caskeys.com>
2018-07-11 11:35:02 -0400

    Add Fontawesome.

--- index.php
+++ index.php
@@ -195,6 +195,9 @@
 		
         <title><?php echo APPLICATION_SETTINGS::NAME; ?></title>        
         
+		<!-- Icon sets -->
+		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+		
         <!-- Latest compiled and minified CSS -->
         <!-- link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css" -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
