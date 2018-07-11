0b33c9fac0f4dad322bcd9fff5b9868c96f6600d
Caskey, Damon V <dc@caskeys.com>
2018-07-11 06:26:47 -0400

    Latest meta tags.

--- index.php
+++ index.php
@@ -189,7 +189,10 @@
 <!DOCtype html>
 <html lang="en">
     <head>
-        <meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1" />
+		<!-- Required meta tags -->
+    	<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		
         <title><?php echo APPLICATION_SETTINGS::NAME; ?></title>        
         
         <!-- Latest compiled and minified CSS -->
